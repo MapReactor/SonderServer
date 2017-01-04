7088e9b9b10b23100f6fe783b3e3f9fd76e2b817
Aaron Trank <aarontrank@gmail.com>
2017-01-04 13:47:48 -0800

    Redix configuration

--- redis/config.js
+++ redis/config.js
@@ -1,10 +1,11 @@
 //For Heroku Deployment
 var URI = require('urijs');
-var uri = URI.parse(process.env.REDIS_URL);
+var config = URI.parse(process.env.REDIS_URL);
+module.exports = config;
 //REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-module.exports = {
-  host: uri.host,
-  port:uri.port,
-  password: uri.password
-};
+// module.exports = {
+//   host: uri.host,
+//   port:uri.port,
+//   password: uri.password
+// };
