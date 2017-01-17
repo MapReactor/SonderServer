55ff018980d12ee174e7142b9f18224a7ce858a5
Aaron Trank <aarontrank@gmail.com>
2017-01-17 12:09:18 -0800

    Settings for deployment to Digital Ocean

--- db/config.js
+++ db/config.js
@@ -1,12 +1,12 @@
 //For Heroku Deployment
-var config = process.env.DATABASE_URL;
-module.exports = config;
+// var config = process.env.DATABASE_URL;
+// module.exports = config;
 
 //For Local Deployment
-// module.exports = {
-//   database: 'sonder',
-//   user: 'postgres',
-//   //password: 'postgres',
-//   port: 32769,
-//   host: '192.168.99.100'
-// };
+module.exports = {
+  database: 'sonder',
+  user: 'postgres',
+  //password: 'postgres',
+  port: 5432,
+  host: 'localhost'
+};
