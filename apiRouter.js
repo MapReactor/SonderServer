c0a9552bc29d18e7e60ff0c64bbff3c3ed1f8cc4
Aaron Trank <aarontrank@gmail.com>
2017-01-17 14:55:40 -0800

    Remove Postgres for deployment on DigitalOcean

--- apiRouter.js
+++ apiRouter.js
@@ -1,13 +1,6 @@
 const apiRouter = require('express').Router();
 const apiController = require('./apiController.js');
 
-apiRouter.get('/users', apiController.getUsers );
-apiRouter.get('/friends/:id', apiController.getFriends );
-apiRouter.get('/history/:id', apiController.getHistory );
-apiRouter.get('/locations/:id', apiController.getLocations );
-
-apiRouter.post('/users', apiController.addUser );
-apiRouter.post('/friends', apiController.updateFriends );
 apiRouter.post('/location', apiController.updateLocation );
 
 
--- redis/config.js
+++ redis/config.js
@@ -2,8 +2,14 @@
 // var config = process.env.REDIS_URL;
 // module.exports = config;
 
+// For DigitalOcean Deployment
+// module.exports = {
+//   host:'localhost',
+//   port:'6379'
+// };
+
 // For Local Deployment
 module.exports = {
-  host:'localhost',
-  port:'6379'
+  host:'192.168.99.100',
+  port:'32768'
 };
