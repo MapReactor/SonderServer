fb2a7cca1f7f48969f4fa57b440a5459bf57af9e
Aaron Trank <aarontrank@gmail.com>
2017-01-17 16:14:19 -0800

    Landing page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const server = require('http').createServer();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 require('./middleware.js')(app, express, server);
 
